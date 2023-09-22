8835bc9540ede587c1c54b837c3c8ea065743048
mohammadreza <spd.mohammadreza@gmail.com>
2023-09-22 04:25:20 -0700

    react router dom add project

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+export default function Home() {
+  return (
+    <div>
+      <Link to={'/courses'}>courses</Link>
+    </div>
+  )
+}
---
+++ src/routes.jsx
@@ -0,0 +1,17 @@
+import Courses from "./pages/Courses/Courses";
+import Home from "./pages/Home/Home";
+
+
+
+
+let routes = [
+    {path:'/' , element: <Home></Home>},
+    {path:'/courses' , element: <Courses></Courses>},
+
+]
+
+
+
+
+
+export default routes
