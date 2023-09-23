b097cdb1e289f55a31c6149e6e0efb910b8617a2
mohammadreza <spd.mohammadreza@gmail.com>
2023-09-23 06:40:29 -0700

    mohammad edit

--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -3,7 +3,7 @@ import Statistic from '../statistic/statistic'
 
 export default function Statistics() {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 items-center  gap-8 mt-10 px-8 '>
+    <div className='grid grid-cols-1 md:grid-cols-2 items-center  gap-8 mt-20 px-8 '>
       <Statistic title={'بازدید'} icon={'tv'} number={'24k+'} Percent={35} bgcolor={'bg-main-red-admin'} txcolor={'text-main-red-admin'} ></Statistic>
       <Statistic title={'به این برنامه رتبه داده اند'} icon={'star'} number={'24k+'} Percent={58} bgcolor={'bg-main-orang-admin'} txcolor={'text-main-orang-admin'}></Statistic>
       <Statistic title={'دانلود ها'} icon={'download'} number={'24k+'} Percent={70} bgcolor={'bg-main-blue-admin'} txcolor={'text-main-blue-admin'}></Statistic>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import SideBar from '../../Components/PanelAdmin/SideBar/SideBar'
+import Statistics from '../../Components/statistics/statistics'
 export default function Home() {
   return (
     <div>
       <SideBar/>
+      <Statistics></Statistics>
     </div>
   )
 }
