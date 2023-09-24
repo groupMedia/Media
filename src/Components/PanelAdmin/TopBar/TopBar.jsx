5868ce9f8051c76f9485b1f8770185dc593b2be1
azizollah <azizollahpayandeh1@gmail.com>
2023-09-24 10:43:34 +0330

    finish

--- src/Components/PanelAdmin/TopBar/TopBar.jsx
+++ src/Components/PanelAdmin/TopBar/TopBar.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import { React, useState } from "react";
+// import TextField from "@mui/material/TextField";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
 
 export default function TopBar() {
   return (
-    <div>
-      <div>
-        
+    <div className="text-white ">
+      <div className="w-[100%] h-[50px] bg-second-bg-web-dark flex">
+        <div>
+
+          <img src="" alt="" />
+          <a href="#">
+            
+            <MailOutlineIcon />
+          </a>
+          <a href="#">
+            
+            <NotificationsNoneIcon />
+          </a>
+          <div className="main w-[300px]  text-white">
+            {/* <h1>React Search</h1>
+            <div className="search h[10px]">
+              <TextField
+                id="outlined-basic"
+                variant="outlined"
+                fullWidth
+                label="Search"
+              />
+            </div> */}
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import TopBar from "./../../Components/PanelAdmin/TopBar/TopBar"
 import SideBar from "../../Components/PanelAdmin/SideBar/SideBar";
 import Statistics from "../../Components/statistics/statistics";
 import DashboardSlider from "../../Components/PanelAdmin/DashboardSlider/DashboardSlider";
 export default function Home() {
   return (
     <div className="">
-
+      <div>
+        <TopBar/>
+      </div>
       <div>
         <SideBar />
       </div>
