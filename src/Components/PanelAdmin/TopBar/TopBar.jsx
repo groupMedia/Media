4422ae40464231ee71ab0f62ac2acf000638a132
azizollah <azizollahpayandeh1@gmail.com>
2023-09-24 13:28:43 +0330

    finish topBar

--- src/Components/PanelAdmin/TopBar/TopBar.jsx
+++ src/Components/PanelAdmin/TopBar/TopBar.jsx
@@ -8,26 +8,26 @@ export default function TopBar() {
     <div className="text-white  ">
       <div className="w-[100%] h-[80px] bg-second-bg-web-dark">
         <div className="flex justify-end items-center gap-7 ml-5">
-          <div className="relative w-[450px]  mt-5">
+          <div className="relative w-[400px]  mt-5">
             <input
               type="text"
-              placeholder="جستجو"
-              className="w-full pl-10 pr-4 py-2 border text-[#fff] bg-[#141414] border-[#141414] rounded-xl outline-none focus:border-[#141414] transition duration-300"
+              placeholder="اینجا جستجو کنید..."
+              className="w-full pl-10 pr-4 text-sm py-2 border text-[#fff] bg-[#141414] border-[#141414] rounded-xl outline-none focus:border-[#141414] transition duration-300"
             />
             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
               <SearchIcon className="text-[#fff]" />
             </div>
           </div>
           <a href="#" className="mt-5">
-            <MailOutlineIcon />
+            <MailOutlineIcon  fontSize="small" className=""/>
           </a>
           <a href="#" className="mt-5">
-            <NotificationsNoneIcon />
+            <NotificationsNoneIcon  fontSize="small" className=""/>
           </a>
           <img
             src="./Images/Profile.jpg"
             alt=""
-            className="rounded-[50%] w-12 mt-4"
+            className="rounded-[50%] w-[45px] mt-4"
           />
         </div>
       </div>
