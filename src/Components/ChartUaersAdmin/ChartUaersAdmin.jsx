303b9a4ec15814fc0da08a2c228a4e43520a1329
mohammadreza <spd.mohammadreza@gmail.com>
2023-09-24 12:45:18 -0700

    mohammad fix

--- src/Components/ChartUaersAdmin/ChartUaersAdmin.jsx
+++ src/Components/ChartUaersAdmin/ChartUaersAdmin.jsx
@@ -11,16 +11,16 @@ import {
 } from "recharts";
 export default function ChartUaersAdmin() {
   const data = [
-    { name: "Group A", value: 400 },
-    { name: "Group B", value: 300 },
-    { name: "Group C", value: 300 },
-    { name: "Group D", value: 200 },
+    { name: "بازدید روزانه", value: 400 },
+    { name: "مشتریان غیر فعال من را با خبر کنید", value: 300 },
+    { name: "مشتریان فعال من را با خبر کنید", value: 300 },
+    { name: "مشتریان جدید", value: 200 },
   ];
   const COLORS = ["#007AFF", "#858EA3", "#F68A04", "#E20E02"];
   return (
     <>
-      <div className="m-8 bg-second-bg-web-dark rounded-md p-2">
-        <div className="text-center text-xl font-bold">
+      <div className="m-8 bg-second-bg-web-dark rounded-md ">
+        <div className="text-center text-xl font-bold py-5 border-b border-b-main-bg-web-dark">
           <h3>آمار کاربران</h3>
         </div>
         <div className="flex justify-center items-center ">
@@ -43,24 +43,26 @@ export default function ChartUaersAdmin() {
             </Pie>
           </PieChart>
         </div>
-        <div className="flex gap-2 mt-8">
-          <div className="flex bg-main-bg-web-dark justify-center items-center gap-4 p-2">
-            <div className="bg-main-red-admin w-5 h-5 rounded-md "></div>
-            <h4>مشتریان جدید</h4>
+        <div className="p-4 md:grid md:grid-cols-2 lg:grid-cols-1  text-sm">
+          <div className="flex flex-col md:flex-row gap-3 mt-8">
+            <div className="flex bg-main-bg-web-dark justify-start items-center gap-4 p-4 rounded-md">
+              <div className="bg-main-red-admin w-5 h-5 rounded-md  "></div>
+              <h4>مشتریان جدید</h4>
+            </div>
+            <div className="flex bg-main-bg-web-dark justify-start items-center gap-4 p-4 rounded-md">
+              <div className="bg-main-orang-admin w-5 h-5 rounded-md "></div>
+              <h4>مشتریان فعال من را با خبر کنید</h4>
+            </div>
           </div>
-          <div className="flex bg-main-bg-web-dark justify-center items-center gap-4 p-4">
-            <div className="bg-main-orang-admin w-5 h-5 rounded-md "></div>
-            <h4>مشتریان فعال من را با خبر کنید</h4>
-          </div>
-        </div>
-        <div className="flex gap-2 mt-8">
-          <div className="flex bg-main-bg-web-dark justify-center items-center gap-4 p-2">
-            <div className="bg-main-blue-admin w-5 h-5 rounded-md "></div>
-            <h4>بازدید روزانه</h4>
-          </div>
-          <div className="flex bg-main-bg-web-dark justify-center items-center gap-4 p-4">
-            <div className="bg-main-gray-admin w-5 h-5 rounded-md "></div>
-            <h4>مشتریان فعال من را با خبر کنید</h4>
+          <div className="flex flex-col md:flex-row gap-3 mt-8">
+            <div className="flex bg-main-bg-web-dark justify-start items-center gap-4 p-4 rounded-md">
+              <div className="bg-main-blue-admin w-5 h-5 rounded-md "></div>
+              <h4>بازدید روزانه</h4>
+            </div>
+            <div className="flex bg-main-bg-web-dark justify-start items-center gap-4 p-4 rounded-md">
+              <div className="bg-main-gray-admin w-5 h-5 rounded-md "></div>
+              <h4>مشتریان غیرفعال من را با خبر کنید</h4>
+            </div>
           </div>
         </div>
       </div>
